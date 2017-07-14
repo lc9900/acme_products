333ee7542f82e0450265f1ce93afa66eb6a555ed
lc9900 <whynot_hm@hotmail.com>
2017-07-14 18:49:50 -0400

    completed product page

--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,15 @@ const db = require('../db');
 module.exports = router;
 
 router.get('/:id', function(req, res){
-    var product = db.findById(req.params.id);
+    var id = parseInt(req.params.id)
+    var product = db.findById(id);
+    // console.log(product);
     res.render('product', product);
 });
+
+router.delete('/:id', function(req, res){
+    var id = parseInt(req.params.id)
+    var product = db.findById(id);
+    db.remove(id);
+    res.redirect('/products');
+})
